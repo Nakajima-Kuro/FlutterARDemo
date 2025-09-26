ab9aefd2892aad8ad994ec0937ad9a30858126b6
Tran Nam Anh <tnnamanh@gmail.com>
2025-09-26 16:27:13 +0700

    Refactor ARHome

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
+import 'package:ar_flutter_plugin/datatypes/hittest_result_types.dart';
+import 'package:ar_flutter_plugin/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin/models/ar_anchor.dart';
+import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
+import 'package:ar_flutter_plugin/models/ar_node.dart';
+import 'package:vector_math/vector_math_64.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ARHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ARHome extends StatefulWidget {
+  const ARHome({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ARHome> createState() => _ARHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ARHomeState extends State<ARHome> {
+  ARSessionManager? _sessionManager;
+  ARObjectManager? _objectManager;
+  ARAnchorManager? _anchorManager;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final List<ARNode> _nodes = [];
+  final List<ARAnchor> _anchors = [];
+
+  @override
+  void dispose() {
+    _sessionManager?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SafeArea(
+        child: ARView(
+          planeDetectionConfig: PlaneDetectionConfig.horizontal,
+          onARViewCreated: _onARViewCreated,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: _anchors.isNotEmpty
+          ? FloatingActionButton.extended(
+              onPressed: _removeEverything,
+              label: const Text('Clear Scene'),
+              icon: const Icon(Icons.delete_outline),
+            )
+          : null,
     );
+  }
+
+  Future<void> _onARViewCreated(
+    ARSessionManager sessionManager,
+    ARObjectManager objectManager,
+    ARAnchorManager anchorManager,
+    ARLocationManager locationManager,
+  ) async {
+    _sessionManager = sessionManager;
+    _objectManager = objectManager;
+    _anchorManager = anchorManager;
+
+    await _sessionManager!.onInitialize(
+      showFeaturePoints: false,
+      showPlanes: true,
+      customPlaneTexturePath: null,
+      showWorldOrigin: false,
+    );
+
+    await _objectManager!.onInitialize();
+    _sessionManager!.onPlaneOrPointTap = _handlePlaneTap;
+  }
+
+  Future<void> _handlePlaneTap(List<ARHitTestResult> hits) async {
+    if (hits.isEmpty) return;
+
+    final planeHit = hits.firstWhere(
+      (hit) => hit.type == ARHitTestResultType.plane,
+      orElse: () => hits.first,
+    );
+
+    final anchor = ARPlaneAnchor(transformation: planeHit.worldTransform);
+    final didAddAnchor = await _anchorManager!.addAnchor(anchor) ?? false;
+    if (!didAddAnchor) return;
+
+    _anchors.add(anchor);
+
+    final node = ARNode(
+      type: NodeType.webGLB,
+      uri:
+          'https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF-Binary/Duck.glb',
+      scale: Vector3.all(0.2),
+      position: Vector3.zero(),
+      rotation: Vector4(0.0, 0.0, 0.0, 1.0),
+    );
+
+    final didAddNode = await _objectManager!.addNode(node, planeAnchor: anchor) ?? false;
+    if (!didAddNode) {
+      await _anchorManager!.removeAnchor(anchor);
+      _anchors.remove(anchor);
+      return;
+    }
+
+    _nodes.add(node);
+    setState(() {});
+  }
+
+  Future<void> _removeEverything() async {
+    for (final node in List<ARNode>.from(_nodes)) {
+      await _objectManager!.removeNode(node);
+      _nodes.remove(node);
+    }
+
+    for (final anchor in List<ARAnchor>.from(_anchors)) {
+      await _anchorManager!.removeAnchor(anchor);
+      _anchors.remove(anchor);
+    }
+
+    setState(() {});
   }
 }
